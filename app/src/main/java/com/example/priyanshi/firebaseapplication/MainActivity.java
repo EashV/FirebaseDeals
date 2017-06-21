7b97d92ae084a3cb71bbb4e92ca6ee07121cc571
Priyanshi <priyanshi@skrilo.com>
2017-06-21 17:56:34 +0530

    firebase appfest app output

--- app/src/main/java/com/example/priyanshi/firebaseapplication/MainActivity.java
+++ app/src/main/java/com/example/priyanshi/firebaseapplication/MainActivity.java
@@ -1,54 +1,94 @@
 package com.example.priyanshi.firebaseapplication;
 
-import com.google.android.gms.ads.AdListener;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.InterstitialAd;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.dynamiclinks.DynamicLink;
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
 
 public class MainActivity extends AppCompatActivity {
     // Remove the below line after defining your own ad unit ID.
     private static final String TOAST_TEXT = "Test ads are being shown. "
             + "To show live ads, replace the ad unit ID in res/values/strings.xml with your own ad unit ID.";
+    private static final String TAG = "MainActivity";
 
-    private static final int START_LEVEL = 1;
-    private int mLevel;
-    private Button mNextLevelButton;
-    private InterstitialAd mInterstitialAd;
     private TextView mLevelTextView;
+    private Button youtubeButton;
+    private Button shareButton;
+    private EditText minText, secText, urlText;
+    private WebView youtubeView;
+    String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // Create the next level button, which tries to show an interstitial when clicked.
-        mNextLevelButton = ((Button) findViewById(R.id.next_level_button));
-        mNextLevelButton.setEnabled(false);
-        mNextLevelButton.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.test_activity);
+        minText = (EditText) findViewById(R.id.time_min);
+        secText = (EditText) findViewById(R.id.time_sec);
+        urlText = (EditText) findViewById(R.id.url_edit);
+        youtubeView = (WebView) findViewById(R.id.youtube_webview);
+        youtubeButton = ((Button) findViewById(R.id.youtube_button));
+        youtubeView.setWebViewClient(new MyBrowser());
+//        url = urlText.getText().toString();
+        minText.clearFocus();
+        secText.clearFocus();
+        youtubeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showInterstitial();
+                showYoutubeVid();
             }
         });
 
-        // Create the text view to show the level number.
-        mLevelTextView = (TextView) findViewById(R.id.level);
-        mLevel = START_LEVEL;
+        shareButton = ((Button) findViewById(R.id.youtube_share_button));
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shareYoutubeVid();
+            }
+        });
 
-        // Create the InterstitialAd and set the adUnitId (defined in values/strings.xml).
-        mInterstitialAd = newInterstitialAd();
-        loadInterstitial();
-
-        // Toasts the test ad message on the screen. Remove this after defining your own ad unit ID.
         Toast.makeText(this, TOAST_TEXT, Toast.LENGTH_LONG).show();
+    }
+
+    private void showYoutubeVid() {
+        url = "https://youtu.be/A0RDIT5-bng";
+        youtubeView.getSettings().setLoadsImagesAutomatically(true);
+        youtubeView.getSettings().setJavaScriptEnabled(true);
+        youtubeView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        youtubeView.loadUrl(url);
+//        Log.i(TAG, youtubeView.getUrl() );
+        //        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://youtu.be/g334Sn9L3rY")));
+    }
+
+    private void shareYoutubeVid() {
+        url = "https://youtu.be/A0RDIT5-bng";
+        String mins = minText.getText().toString();
+        String secs = secText.getText().toString();
+        String test = url + "?t=" + mins + "m" + secs + "s";
+
+//        urlText = "https://m273s.app.goo.gl/rniX";
+
+        DynamicLink dynamicLink = FirebaseDynamicLinks.getInstance().createDynamicLink()
+                .setLink(Uri.parse(url))
+                .setDynamicLinkDomain("m273s.app.goo.gl/")
+                .buildDynamicLink();
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_SUBJECT, "Check out this video");
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "Hey start playing this video: " + "m273s.app.goo.gl/rniX");
+        sendIntent.setType("text/plain");
+        startActivity(Intent.createChooser(sendIntent, "Share via"));
     }
 
 
@@ -73,51 +113,11 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private InterstitialAd newInterstitialAd() {
-        InterstitialAd interstitialAd = new InterstitialAd(this);
-        interstitialAd.setAdUnitId(getString(R.string.interstitial_ad_unit_id));
-        interstitialAd.setAdListener(new AdListener() {
-            @Override
-            public void onAdLoaded() {
-                mNextLevelButton.setEnabled(true);
-            }
-
-            @Override
-            public void onAdFailedToLoad(int errorCode) {
-                mNextLevelButton.setEnabled(true);
-            }
-
-            @Override
-            public void onAdClosed() {
-                // Proceed to the next level.
-                goToNextLevel();
-            }
-        });
-        return interstitialAd;
-    }
-
-    private void showInterstitial() {
-        // Show the ad if it's ready. Otherwise toast and reload the ad.
-        if (mInterstitialAd != null && mInterstitialAd.isLoaded()) {
-            mInterstitialAd.show();
-        } else {
-            Toast.makeText(this, "Ad did not load", Toast.LENGTH_SHORT).show();
-            goToNextLevel();
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
         }
-    }
-
-    private void loadInterstitial() {
-        // Disable the next level button and load the ad.
-        mNextLevelButton.setEnabled(false);
-        AdRequest adRequest = new AdRequest.Builder()
-                .setRequestAgent("android_studio:ad_template").build();
-        mInterstitialAd.loadAd(adRequest);
-    }
-
-    private void goToNextLevel() {
-        // Show the next level and reload the ad to prepare for the level after.
-        mLevelTextView.setText("Level " + (++mLevel));
-        mInterstitialAd = newInterstitialAd();
-        loadInterstitial();
     }
 }
